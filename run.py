7105105519bd7081fd479c2580a9eec866aa5af5
OmPrakash Patel <omprakashpatelcs@gmail.com>
2016-02-04 12:01:01 +0530

    Removing unec code

--- run.py
+++ run.py
@@ -1,14 +1,6 @@
 from todoapp import app, urls
-from gevent import monkey;
-from gevent.wsgi import WSGIServer
 
 if __name__ == "__main__":
-    print "GEvent Started"
-    monkey.patch_all()
-    http_server = WSGIServer(('',8000), app)
-    http_server.serve_forever()
-
-# if __name__ == "__main__":
-# 	app.debug = True
-# 	app.run(host=app.config['HOST'], port=app.config['PORT'])
+	app.debug = True
+	app.run(host=app.config['HOST'], port=app.config['PORT'])
 
